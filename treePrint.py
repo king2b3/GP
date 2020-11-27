04bd739422ace2e9ab8de63ca7ff8ca188c66aad
king2b3 <king2b3@mail.uc.edu>
2020-11-27 17:35:12 -0500

    update to tree print with operations
    doc string and comments added
    operations.py intergration added

--- treePrint.py
+++ treePrint.py
@@ -7,47 +7,54 @@
         pip3 install graphviz
 '''
 
+import operations as op
 
-def checkDuoGate(new_l,ast,bin_ops,solo_ops):
-    #print('here is the new_l',new_l)
+def checkDuoGate(new_l,ast,
+                 bin_ops=op.binary_operators.keys(),
+                 solo_ops=op.solo_operators.keys()
+                ):
+    ''' Checks if the current node is a duo gate or not
+    '''
+    # checks what the current node is
     if ast[new_l] in bin_ops:
-        #print('checkDuoGate found bin op for child 1: ',ast[new_l])
         temp_l = checkDuoGate(new_l + 1,ast,bin_ops,solo_ops)
     elif ast[new_l] in solo_ops:
-        #print('checkDuoGate found solo op for child 1:',ast[new_l])
         temp_l = checkSoloGate(new_l + 1,ast,bin_ops,solo_ops)
     else:
-        #print('child 1 is not a gate')
+        # if not a bin or solo op, then look at the next child
         temp_l = new_l + 1
 
-    #print('temp_l from inside the loop part 1: ',temp_l)
     if ast[temp_l] in bin_ops:
-        #print('checkDuoGate found bin op for child 2')
         temp_l = checkDuoGate(temp_l + 1,ast,bin_ops,solo_ops)   
     elif ast[temp_l] in solo_ops:
-        #print('checkDuoGate found solo op for child 2')
         temp_l = checkSoloGate(temp_l + 1,ast,bin_ops,solo_ops)   
     else:
-        #print('child 2 is not a gate')
+        # check the next node to be a child
         temp_l += 1
     
-    #print('temp_l from inside the loop part 2: ',temp_l+new_l)
     return temp_l
 
-def checkSoloGate(new_l,ast,bin_ops,solo_ops):
+def checkSoloGate(temp_l,ast,
+                  bin_ops=op.binary_operators.keys(),
+                  solo_ops=op.solo_operators.keys()
+                 ):
+    ''' Checks if the current node is a solo gate
+    '''
     if ast[new_l] in bin_ops:
-        temp_l = checkDuoGate(new_l+1,ast,bin_ops,solo_ops)
-        #print('temp_l from solo ops bin check: ',temp_l)
-        return temp_l
-
+        temp_l = checkDuoGate(temp_l+1,ast,bin_ops,solo_ops)
     elif ast[new_l] in solo_ops:
-        temp_l = checkSoloGate(new_l+1,ast,bin_ops,solo_ops)
-        #print('temp_l from solo ops solo check: ',temp_l)
-        return temp_l 
+        temp_l = checkSoloGate(temp_l+1,ast,bin_ops,solo_ops)
     else:
-        return new_l + 1
+        temp_l += 1
+    return temp_l
 
-def treePrint(ast,bin_ops,solo_ops,file_name='test-output.gv'):
+def treePrint(ast,
+              file_name='test-output.gv',
+              bin_ops=op.binary_operators.keys(),
+              solo_ops=op.solo_operators.keys()
+             ):
+             ''' Prints ast to a pdf file
+             '''
     from graphviz import Digraph
 
     u = Digraph('unix',filename=file_name,
@@ -56,14 +63,18 @@ def treePrint(ast,bin_ops,solo_ops,file_name='test-output.gv'):
 
     ast_nums = []
 
-    count_g = 0 
-    count_i = 0
+    count_g = 0 # number of gate counter
+    count_i = 0 # number of input counter
+    # steps through the AST
     for l in range(len(ast)):
         if ast[l] in bin_ops or ast[l] in solo_ops:
+            # names the node as a gate_gateCounter
             temp = 'gate_' + str(count_g)
             ast_nums.append(temp)
+            # updates counter
             count_g += 1
         else:
+            # names the node an input_insCounter
             temp = 'ins_' + str(count_i)
             ast_nums.append(temp)
             count_i += 1
@@ -71,16 +82,18 @@ def treePrint(ast,bin_ops,solo_ops,file_name='test-output.gv'):
     zipped_ast = list(zip(ast,ast_nums))
     count = 0
     for node in zipped_ast:
+        # creates the nodes 
         u.node(node[1],node[0])
         count += 1
 
+    # draws the lines between the nodes
     for l in range(len(ast)):
         if ast[l] in bin_ops:
-            # next value is child
-            # check conditions for second child
             u.edge(zipped_ast[l][1],zipped_ast[l+1][1])
             if ast[l+1] in bin_ops:
                 temp_l = checkDuoGate(l+2,ast,bin_ops,solo_ops)
+                # temp_l contains the locations in the AST of this node's children since it is an operator. This only isn't true when the node is an input
+                # zipped_ast[l][1] is the first child by default, zipped_ast[temp_l][1] is the second child
                 u.edge(zipped_ast[l][1],zipped_ast[temp_l][1])
             elif ast[l+1] in solo_ops:
                 temp_l = checkSoloGate(l+2,ast,bin_ops,solo_ops)
@@ -89,9 +102,11 @@ def treePrint(ast,bin_ops,solo_ops,file_name='test-output.gv'):
                 u.edge(zipped_ast[l][1],zipped_ast[l+2][1])
 
         elif ast[l] in solo_ops:
+            # next node in ast is it's child
             u.edge(zipped_ast[l][1],zipped_ast[l+1][1])
 
         else:
+            # no need to draw a line if its a input
             pass
 
     u.render()
